e456e3308280508dc5f60a181f2a6b81964ec414
Michel Adan <m.adan@rsegrp.com>
2018-12-18 19:41:24 -0800

    Initial Commit

---
+++ src/main/java/com/rsegrp/spring5recipeapp/Spring5RecipeMongoApplication.java
@@ -0,0 +1,14 @@
+package com.rsegrp.spring5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipeMongoApplication.class, args);
+	}
+
+}
+
